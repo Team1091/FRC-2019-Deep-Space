aff94b3b1d36cab97681e12d5d60a2c6ed0e8d56
Aut0P1ay <Mr.bansheep@gmail.com>
2019-02-18 19:55:15 -0600

    Reversed vision communication

--- robot/src/main/java/frc/robot/components/TargetingSystem.java
+++ robot/src/main/java/frc/robot/components/TargetingSystem.java
@@ -1,47 +1,43 @@
 package frc.robot.components;
 
-import com.google.gson.Gson;
 import com.team1091.shared.control.ImageInfo;
 import com.team1091.shared.system.ITargetingSystem;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.ConnectException;
-import java.net.URL;
+import static spark.Spark.*;
 
 public class TargetingSystem implements ITargetingSystem {
 
-    private static final Gson gson = new Gson();
-    private URL visionURL;
+    //  private static final Gson gson = new Gson();
+    //    private URL visionURL;
     private ImageInfo imageInfo = new ImageInfo();
 
-    private final Runnable visionUpdater = () -> {
-        while (true) {
-            try (
-                    InputStreamReader isr = new InputStreamReader(visionURL.openStream());
-                    BufferedReader in = new BufferedReader(isr)
-            ) {
-                imageInfo = gson.fromJson(in.readLine(), ImageInfo.class);
-//                System.out.println("Vision: " + imageInfo.center);
-                Thread.sleep(100);
-            } catch (ConnectException e) {
-                System.out.println("No connection");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    };
+//    private final Runnable visionUpdater = () -> {
+//        while (true) {
+//            try (
+//                    InputStreamReader isr = new InputStreamReader(visionURL.openStream());
+//                    BufferedReader in = new BufferedReader(isr)
+//            ) {
+//                imageInfo = gson.fromJson(in.readLine(), ImageInfo.class);
+////                System.out.println("Vision: " + imageInfo.center);
+//                Thread.sleep(100);
+//            } catch (ConnectException e) {
+//                System.out.println("No connection");
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    };
+//
+//    private Thread thread = new Thread(visionUpdater);
 
-    private Thread thread = new Thread(visionUpdater);
-
-    public TargetingSystem() {
-        try {
-            visionURL = new URL("http://169.254.152.146:4567/center");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public TargetingSystem() {
+//        try {
+//            visionURL = new URL("http://169.254.152.146:4567/center");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     @NotNull
     @Override
@@ -51,7 +47,34 @@ public class TargetingSystem implements ITargetingSystem {
 
     @Override
     public void start() {
-      //  thread.start();
+        //  thread.start();
+        port(5801);
+        exception(Exception.class, (exception, request, response) -> exception.printStackTrace());
+
+        get("/center", (req, res) -> {
+            System.out.println("Got Here");
+//            if (req == null) {
+//                System.out.println("Its null");
+//            }
+//            if (req.params() == null) {
+//                System.out.println("Params null");
+//            }
+//            for (String o : req.params().values()) {
+//                System.out.print(o);
+//            }
+
+            imageInfo = new ImageInfo(
+                    Boolean.parseBoolean(req.queryParams("seen")),
+                    Double.parseDouble(req.queryParams("center")),
+                    Double.parseDouble(req.queryParams("distance"))
+            );
+            return "ok " + imageInfo;
+        });
+
+//        exception(Exception::class.java) { exception, request, response -> exception.printStackTrace() }
+
+        // a little webserver.  Go to http://localhost:4567/center
+//        get("/center") { _, _ -> gson.toJson(imageInfo) }
     }
 
 }
